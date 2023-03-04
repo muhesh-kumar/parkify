3ae43949bfab164276dd2f675858ebfdd35548e4
Muhesh Kumar <muhesh4102002@gmail.com>
2023-03-04 11:24:58 +0530

    feat: add get and set queries in redis

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,65 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const { Server } = require('socket.io');
+const { createClient } = require('redis');
+const { promisify } = require('util');
+
+const redisClient = createClient({
+  password: 'hrjvcSFOKrqNvnHUXwthokI9SF6l5Rtp',
+  socket: {
+    host: 'redis-17510.c301.ap-south-1-1.ec2.cloud.redislabs.com',
+    port: 17510,
+  },
+});
+
+async function connectRedisClient() {
+  await redisClient.connect();
+}
+connectRedisClient();
+
+// add a sample json to a redis DB with error handling
+async function addJsonToRedis(key, json) {
+  try {
+    const response = await redisClient.set(key, JSON.stringify(json));
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const carData = {
+  carImageLocation: '/images/cars/ABC123.png',
+  entryTimeStamp: '2022-02-28T10:00:00Z',
+};
+
+// addJsonToRedis('ABC123', carData);
+
+// delete a key from a redis DB with error handling
+async function deleteKeyFromRedis(key) {
+  try {
+    const response = await redisClient.del(key);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+// deleteKeyFromRedis('car_entry_data');
+
+// get all the keys and its corresponding value from a redis DB with error handling
+async function getAllKeysFromRedis() {
+  try {
+    const keys = await redisClient.keys('*');
+    const values = await Promise.all(keys.map((key) => redisClient.get(key)));
+    const result = keys.reduce((acc, key, index) => {
+      acc[key] = JSON.parse(values[index]);
+      return acc;
+    }, {});
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+// getAllKeysFromRedis();
 
 const HttpError = require('./utils/http-error');
 
